1ed38a8bf3984caa93cfcb431db266a2ca10c4ef
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-10-04 23:55:12 +0700

    Dark.py

---
+++ Dark.sh
@@ -0,0 +1,92 @@
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+figlet Dark FB | lolcat
+
+sleep 1
+
+echo "=========================================" | lolcat
+echo "Author  : Hacker20" | lolcat
+echo "Youtube : https://my channel/Sandi Yt" | lolcat
+echo "GiHub   : https://github.com/hecker20" | lolcat
+echo "=========================================" | lolcat
+echo
+sleep 1
+echo
+echo "Login Akun Facebook [+]" | lolcat
+echo
+sleep 1
+echo -n $cyan"Ussername : " ;read d
+echo
+sleep 1
+echo -n $cyan"Password : " ;read p
+echo
+#/sdcard
+sleep 1
+echo "Sedang Login [+]" | lolcat
+echo
+sleep 3
+echo "10%" | lolcat
+sleep 3
+echo "20%" | lolcat
+sleep 3
+echo "30%" | lolcat
+sleep 3
+echo "40%" | lolcat
+sleep 3
+echo "50%" | lolcat
+sleep 3
+echo "60%" | lolcat
+sleep 3
+echo "70%" | lolcat
+sleep 3
+echo "80%" | lolcat
+sleep 3
+echo "90%" | lolcat
+sleep 3
+echo "100%" | lolcat
+sleep 4
+echo
+echo "Login Selesai [✓]" | lolcat
+echo
+sleep 2
+clear
+figlet CRARCK | lolcat
+echo
+echo "Crack Runing...[/]" | lolcat
+sleep 4
+echo "=======================================" | lolcat
+sleep 8
+echo
+echo "---------------------------------------" | lolcat
+echo "          Email      |     Password     " | lolcat
+echo "---------------------------------------"  | lolcat
+echo "  089758162567       |  kunyuk         " | lolcat
+echo "  083154415561       |  dwiutomo       " | lolcat
+echo "  085695474482       |  zero one       " | lolcat
+echo "  089888283914       |  adun1303       " | lolcat
+echo "  082362284663       |  arnold14       " | lolcat
+echo "  082362284663       |  sinaga14       " | lolcat
+echo "  083812515636       |  zhaki123       " | lolcat
+echo "  083163540994       |  hengkykancil   " | lolcat
+echo "  081277551898       |  soedirman      " | lolcat
+echo "  085232336378       |  jancok         " | lolcat
+echo "  089800034891       |  ariessetiawan  " | lolcat
+echo "  085517528294       |  Dini Sayang    " | lolcat
+echo "  082272107299       |  mawarmerah123  " | lolcat
+echo "  083148779615       |  pahrur         " | lolcat
+echo "  085693024535       |  BANGSAT        " | lolcat
+echo "  085872355170       |  gabungan       " | lolcat
+echo "  085640508130       |  bajingansu     " | lolcat
+echo "  082395879343       |  Rizkisef       " | lolcat
+echo "  081357707303       |  adesetyomw     " | lolcat
+echo "---------------------------------------" | lolcat
+sleep 7
+figlet SUKSES | lolcat
